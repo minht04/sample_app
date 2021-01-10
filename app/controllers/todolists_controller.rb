2815f4618e0cbf316d61ae56046cc42d291e34c0
EC2 Default User <ec2-user@ip-172-31-43-32.ec2.internal>
2021-01-10 05:15:25 +0000

    [Finish] 2-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,9 +1,9 @@
 class TodolistsController < ApplicationController
-  
+
   def new
     @list = List.new
   end
-  
+
   def create
     # 1.データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -11,11 +11,11 @@ class TodolistsController < ApplicationController
     list.save
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
@@ -31,7 +31,7 @@ class TodolistsController < ApplicationController
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7d9f7c2e898d597222793bead3b14b63a0e52a82f800ed1eced1a1df3040ce4f437a750463764d66a858a72be6c50c96f8b275e5dc13b40a5e1643247ea05a79'
