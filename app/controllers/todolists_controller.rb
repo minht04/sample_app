40a3c40b5e234e956cb767c09b213e0777f353a8
EC2 Default User <ec2-user@ip-172-31-43-32.ec2.internal>
2021-01-28 07:23:08 +0000

    Update

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,4 @@
 class TodolistsController < ApplicationController
-
   def new
     @list = List.new
   end
@@ -19,25 +18,27 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
   def destroy
-    list = List.find(params[:id]) #データ（レコード）を1件取得
-    list.destroy #データ（レコード）を削除
-    redirect_to todolists_path # 投稿一覧画面へリダイレクト 
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-
 end
-
